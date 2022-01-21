c6b3fccbd8605d50bac466681cdf9f69e4e3a96f
jop <jop@st-andrews.ac.uk>
2022-01-21 19:34:43 +0000

    Added resource for MSC classes

--- src/api.py
+++ src/api.py
@@ -1,33 +1,56 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from lxml import etree
+import requests
 from scraper import Scraper
 
 app = Flask(__name__)
 api = Api(app)
 
-ROOT = "https://zbmath.org/?q=an:"
+RECORD_ROOT = "https://zbmath.org/?q=an:"
+API_ROOT = "https://oai.zbmath.org/v1/?verb="
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello':'world'}
-
-    # Currently returns entire text string of xml from API call
-    # ID is DE number for the record, not the Zbl code
 class Records(Resource):
     """Resource for retrieving zbMATH records via webscraping"""
     def get(self, id):
         """Retrieves the key fields from a given zbMATH record
         
         Args:
-            id: The DE number of the record in question (Zbl code also accepted)
+            id: The DE number of the record in question
         
         Returns: JSON serializable dictionary representation of the records key fields
         """
-        s = Scraper(f'{ROOT}{id}')
+        s = Scraper(f'{RECORD_ROOT}{id}')
         return s.getInfoJSON()
 
-api.add_resource(HelloWorld, '/')
+class Classes(Resource):
+    """Resource for retrieving available MSC codes using zbMATH api"""
+    def get(self):
+        """Retrives all available 2 digit MSC codes on zbMATH"""
+        PAGES = 7 # 64 MSC codes and 10 per request (page)
+        classes = {}
+        for i in range(PAGES):
+            if i == 0:
+                xml = requests.get("https://oai.zbmath.org/v1/?verb=ListSets").content
+            else:
+                xml = requests.get(f"https://oai.zbmath.org/v1/?verb=ListSets&resumptionToken={token}").content
+            root = etree.fromstring(xml)
+            sets = root[2]
+
+            for j in range(len(sets)-1):
+                s = sets[j]
+                classes[s[0].text] = s[1].text[6:]
+
+            token = sets[-1].text 
+            print(f"Page {i+1} complete")
+        # Add the last item on the last page
+        s = sets[-1]
+        classes[s[0].text] = s[1].text
+
+        return classes
+
 api.add_resource(Records, '/records/<int:id>')
+api.add_resource(Classes, '/classes')
 
 if __name__ == "__main__":
     app.run(debug=True)
