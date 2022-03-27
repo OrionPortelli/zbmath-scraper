235e34a51be004f75971fd3d72af30f0248ccda3
jop <jop@st-andrews.ac.uk>
2022-03-27 20:25:40 +0100

    Restructured scripts

--- scripts/full-script.py
+++ scripts/full-script.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # Module uses reset skip to collect all records in the desired filters
-from api.py_client import fullCollect, cleanDataset
+from src.api.py_client import fullCollect, cleanDataset
 
 # Gets a dirty date
 fullCollect(idpath='data/clean_id.json', recordpath='data/clean_record.json', set='20', start='2013-11-28', end='2013-11-30')
 
 # Cleans a dirty date
-#cleanDataset(inpath='data/clean_record.json', outpath='data/clean_record.json')
+cleanDataset(inpath='data/clean_record.json', outpath='data/clean_record.json')
