ac9e654a886da7c3a087899193c41453c93adfab
jop <jop@st-andrews.ac.uk>
2022-03-27 19:32:26 +0100

    Drafted setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs4098-analytics',
+    version='1.0',
+    description='Web scraping API for zbMATH records info (including software)',
+    author='Orion Portelli',
+    author_email='jeffersonorionportelli@gmail.com',
+    packages=find_packages()
+)
